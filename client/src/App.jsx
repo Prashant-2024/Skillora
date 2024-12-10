e5c45cc968d07e061a0b69ca5294d52c0452eb25
Prashant Solanki <prashantsolanki2025@gmail.com>
2024-12-10 06:56:08 +0530

    Hero Section - Done

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Login from "./pages/Login";
 import Navbar from "./components/Navbar";
+import Hero from "./pages/student/Hero";
 
 const App = () => {
   return (
     <main>
       <Navbar />
+      <Hero />
       <Login />
     </main>
   );
--- client/src/pages/student/Hero.jsx
+++ client/src/pages/student/Hero.jsx
@@ -1,7 +1,36 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 import React from "react";
 
 const Hero = () => {
-  return <div>Hero</div>;
+  return (
+    <div className="relative bg-gradient-to-r from-blue-500 to bg-indigo-600 dark:to-gray-900 py-24 px-4 text-center">
+      <div className="max-w-3xl mx-auto">
+        <h1 className="text-white text-4xl font-bold mb-4">
+          Find the Best Courses for you
+        </h1>
+        <p className="text-gray-200 dark:text-gray-400 mb-8">
+          Discover, Learn and Upkill with our wide range of courses
+        </p>
+
+        <form
+          action=""
+          className="flex items-center justify-center bg-white dark:bg-gray-800 rounded-full shadow-lg overflow-hidden max-w-xl mx-auto mb-6"
+        >
+          <Input
+            type="text"
+            className="flex-grow bg-white border-none focus-visible:ring-0 px-6 py-3 text-gray-900 dark:text-gray-100 placeholder-gray-400 dark:placeholder-gray-500"
+          />
+          <Button className="bg-blue-600 dark:bg-blue-700 text-white px-6 py-3 rounded-r-full hover:bg-blue-700 dark:hover:bg-blue-800">
+            Search
+          </Button>
+        </form>
+        <Button className="bg-white dark:bg-gray-800 text-blue-600 rounded-full hover:bg-gray-200">
+          Explore Courses
+        </Button>
+      </div>
+    </div>
+  );
 };
 
 export default Hero;
